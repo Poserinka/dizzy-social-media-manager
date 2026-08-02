b380b827e362b500b62d0c94caa25f968c141ea3
Poserinka <111547033+Poserinka@users.noreply.github.com>
2026-08-03 00:47:06 +0200

    Fix Social Media autoloader namespace

--- dizzy-social-media-manager.php
+++ dizzy-social-media-manager.php
@@ -3,7 +3,7 @@
  * Plugin Name: Dizzy Social Media Manager
  * Plugin URI: https://github.com/Poserinka/dizzy-social-media-manager
  * Description: Poster generation and social media exports for Dizzy Events Manager.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Poserinka Design
  * Text Domain: dizzy-social-media-manager
  * Requires PHP: 8.2
@@ -14,7 +14,7 @@ declare(strict_types=1);
 
 defined('ABSPATH') || exit;
 
-define('DIZZY_SOCIAL_VERSION', '1.0.0');
+define('DIZZY_SOCIAL_VERSION', '1.0.1');
 define('DIZZY_SOCIAL_PATH', plugin_dir_path(__FILE__));
 define('DIZZY_SOCIAL_URL', plugin_dir_url(__FILE__));
 
